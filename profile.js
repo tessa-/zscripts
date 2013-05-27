0d38089cdaba8bb5fa4f8eb8f4946636e5b79220
ArchZombie <archzombielord@gmail.com>
2013-05-27 17:17:21 -0400

    v0.0.3

---
+++ profile.js
@@ -0,0 +1,85 @@
+({
+require: ["io"]
+, 
+database: new Object
+
+/* <Object
+profiles: <Object Key:[<Int indexForProfile>] Value:[<Object profile>] >
+,
+profile_counter: <INT>
+> */
+,
+relationaldatabase: new Object
+/* <Object
+names: <Object Key:[<String "$"> + <String name>] Value:[<Int indexForProfile>] >
+,
+ips: <Object Key:[<String ipaddr>] Value:[<Int indexForProfile>] >
+*/
+
+
+
+,
+
+loadModule: function ()
+{
+    this.database = scirpt.module.io.read("profile");
+
+    this.relationaldatabase.ips = new Object;
+    this.relationaldatabase.names = new Object;
+    this.relationaldatabase.teams = new Object;
+
+    if (!this.database.profiles) this.database.profiles = new Object;
+
+    var this_database_profiles = this.database.profiles;
+    for (var x1 in this_database_profiles)
+    {
+        var this_database_profiles__x1 = this_database_profiles[x1];
+
+        var this_database_profiles__x1_names = this_database_profiles__x1.names || [];
+        var this_database_profiles__x1_ips = this_database_profiles__x1.ips || [];
+        // ignored for now // var this_database_profiles__x1_teams = this_database_profiles__x1.teams || [];
+
+        for (var x2 in this_database_profiles__x1_names)
+        {
+            this.relationaldatabase.names[this_database_profiles__x1_names[x2]] = x1;
+        }
+
+        for (var x2 in this_database_profiles__x1_ips)
+        {
+            this.relationaldatabase.ips[this_database_profiles__x1_ips[x2]] = x1;
+        }
+        
+    }
+}
+,
+profilePlayer: function (src)
+{
+    var name = sys.name(src).toLowerCase();
+    var ip = sys.ip(src);
+    var team = null;
+    var matches = {};
+
+    if (ip in database.ips)
+    {
+        matches[database.ips[ip]] = true;
+    }
+
+    if ("$"+name in relationaldatabase.names)
+    {
+        matches[database.names["$"+name]] = true;
+    }
+
+    if (team != null && team in database.teams)
+    {
+        null;
+    }
+
+    
+}
+,
+unloadModule: function ()
+{
+
+}
+
+});
--- security.js
+++ security.js
@@ -7,34 +7,7 @@
     ,
     loadModule: function () 
     {
-	var now = +new Date;
-
         this.database = script.module.io.read("security");
-        var _this = this;
-        function autosave ()
-        {
-            script.module.io.write("security", script.module.security.database);
-        }
-        sys.setTimer(autosave, 60000, true);
-
-	if (!this.database.bans) this.database.bans = new Object;
-
-	if (!this.database.ban_counter) this.database.ban_counter = 0;
-
-        for (var x in this.database.bans)
-        {
-            if (this.database.bans[x].expires)
-            {
-		if (this.database.bans[x].expires >= now) this.invalidateBan(x);
-                
-		else
-		{
-		    script.module.sched.at(this.database.bans[x].expires, function () {
-			if (script.module.security.database.bans[x].expires >= +(new Date) - 1) delete script.module.security.database.bans[x];
-                    });
-		}
-            }
-        }
     }
     ,
     unloadModule: function()
@@ -44,7 +17,6 @@
     ,
     registerBan: function (ban)
     {
-	database.bans
     }
     
 })
