7a76b9f6b19a0065320736bd8982e58b86fca220
ArchZombie <archzombielord@gmail.com>
2013-05-31 18:02:06 -0400

    Add draft of commandparser.js

---
+++ parsecommand.js
@@ -0,0 +1,46 @@
+({
+    require: []
+    ,
+    loadModule: function()
+    {
+       
+    }
+    ,
+    parseCommand: function(text)
+    {
+        var cmd = new Object;
+        
+        var match = text.match(/^(?:!|\/)([^\s]*)\s+(.*)/i);
+
+        cmd.name = match[1];
+        cmd.input = match[2];
+
+        cmd.args = [];
+        cmd.flags = {};
+
+        var input = cmd.input;
+
+        while (input != (input =
+                         input.replace(/^\s*(?:\-{1,2}((?:\w|\\ )+)(?:\=(?:\"((?:\\.|[^\"])+)\"|((?:\w|\\ )+)))?)|(?:(?:\"((?:\\.|[^\"])+)\")|((?:\w|\\ )+))/, cl_next))
+               //                                      ^flagname              ^flagvalstr        ^flagvalueraw             ^argvalstr         ^argvalraw
+              ) {};
+        
+        function cl_next (text, flagname, flagvaluestr, flagvalueraw, argvaluestr, argvalueraw)
+        {
+            script.module.com.broadcast(JSON.stringify([text, flagname, flagvaluestr, flagvalueraw/* arguments[4], arguments[5]*/]));
+            if (flagname)
+            {
+                cmd.flags[flagname] = ((flagvaluestr ? flagvaluestr: void 0) || flagvalueraw).replace(/\\(.)/g, "$1");
+            }
+            else 
+            {
+                cmd.args.push(((argvaluestr? argvaluestr: void 0) || argvalueraw).replace(/\\(.)/g, "$1"));
+            }
+            return "";
+        }
+
+        return cmd;
+        
+    }
+
+});
