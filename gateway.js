4c94d742c618b31e0aada781e5e7b555a18ecdc6
ArchZombie <archzombielord@gmail.com>
2013-05-31 23:12:13 -0400

    Update gateway.js greeting

--- gateway.js
+++ gateway.js
@@ -1,5 +1,5 @@
 ({
-    require: ["security", "profile","com", "parsecommand"]
+    require: ["security", "profile", "com", "parsecommand"]
     ,
     loadModule: function()
     {
@@ -10,15 +10,19 @@
     ,
     beforeLogIn: function(src)
     {
-
-
+        
     }
-
     ,
     afterLogIn: function(src)
     {
+        var os = sys.os(src);
         script.log(script.module.profile.profileOpenCreate(src));
-        script.module.com.broadcast("Hello " + sys.name(src) +"! You use " + sys.os(src) +  " eh? Commands don't work yet, don't complain the scripts are new!" );
+        script.module.com.broadcast("Hello " + sys.name(src) +"! Commands don't work yet, don't complain the scripts are new!" );
+
+        if (os === "linux")
+        {
+            script.module.com.broadcast(sys.name(src) +", I see you are a linux user! You are enlightened!");
+        }
     }
 
 });
