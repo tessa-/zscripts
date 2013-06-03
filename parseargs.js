9342ad842112d0611f96bf234d059ddb6ce21e86
ArchZombie <ryan@tux-with-jetpack.(none)>
2013-06-02 22:14:37 -0400

    Add freewrite draft for parse args

---
+++ parseargs.js
@@ -0,0 +1,34 @@
+({
+    require: ["profile"]
+    ,
+    parseArgs: function (args)
+    {
+	var retval = {
+	    profs: []
+	    ,
+	    uids: []
+	};
+
+	for (var x in args)
+	{
+	    if (args[x].length == 0) continue;
+
+	    if (args[x][0] == "?")
+	    {
+		var m = args[x].match(/^\?(\w+):(.+)$/);
+
+                if (!m) continue;
+
+                if (m[1] == "prof")
+                {
+                    if ( m[2] in this.module.profile.database.profiles)
+                    {
+                        profs.push (m[2]);
+                    }
+                }
+	    }
+            
+	}
+        return retval;
+    }
+});
