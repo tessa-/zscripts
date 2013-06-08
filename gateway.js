4bffb4ec071ac7513a31135cae6cb6742e84e332
ArchZombie <archzombielord@gmail.com>
2013-06-08 14:47:12 -0400

    Make gateway more resistant to attack

--- gateway.js
+++ gateway.js
@@ -8,28 +8,37 @@
     ,
     beforeLogIn: function(src)
     {
-        var prof = this.profile.profileOpenCreate(src);
+        var profs = this.profile.profileMatches(src);
         
-        if (sys.auth(src) != 3 && this.security.profIsBanned(prof))
+        for (var x in profs)
         {
-            var ban = this.security.getBan(prof);
-            
-            this.com.message(
-                [src],
-                "You are banned until: "+
-                    (ban.expires ? new Date(ban.expires).toString() + " ("+this.time.diffToStr(ban.expires - +new Date)  +" from now)" : "indefinite" )+
-                    " Reason: " + ban.reason +
-                    " Ban Author: " + ban.author
-                ,
-                this.theme.CRITICAL
-            );
-
-            this.logs.logMessage(this.logs.WARN, "Banned user: " + sys.name(src) + " (IP: " + sys.ip(src) + ") (#: " + prof + ") tried to log in.");
-            
-            sys.stopEvent();
-            
-            return;
+            var prof = profs[x];
+            if (sys.auth(src) != 3 && this.security.profIsBanned(prof))
+            {
+                var ban = this.security.getBan(prof);
+                
+                this.com.message(
+                    [src],
+                    "You are banned until: "+
+                        (ban.expires ? new Date(ban.expires).toString() + " ("+this.time.diffToStr(ban.expires - +new Date)  +" from now)" : "indefinite" )+
+                        " Reason: " + ban.reason +
+                        " Ban Author: " + ban.author
+                    ,
+                    this.theme.CRITICAL
+                );
+                
+                this.logs.logMessage(this.logs.WARN, "Banned user: " + sys.name(src) + " (IP: " + sys.ip(src) + ") (#: " + prof + ") tried to log in.");
+                
+                sys.stopEvent();
+                
+                return;
+            }
         }
+        this.logs.logMessage(this.logs.INFO, sys.name(src) + " logged in.");
     }
-
+    ,
+    beforeLogOut: function (src)
+    {
+        this.logs.logMessage(this.logs.INFO, sys.name(src) + " logged out.");
+    }
 });
