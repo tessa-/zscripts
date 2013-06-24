4b3acfa84d02845fa287005f7592369997a65eae
ArchZombie <archzombielord@gmail.com>
2013-06-24 00:45:38 -0400

    /authlist

---
+++ authlist_command.js
@@ -0,0 +1,39 @@
+({
+
+    require:
+    [
+        "commands"
+        ,
+        "com"
+        ,
+        "theme"
+        ,
+        "text"
+    ]
+    ,
+    loadModule: function ()
+    {
+        this.commands.registerCommand("authlist", this);
+    }
+    ,
+    authlist: 
+    {
+        desc: "Lists the auth on the server."
+        ,
+        aliases: ["auths"]
+        ,
+        perm: function () { return true; }
+        ,
+        code: function () 
+        {
+            this.com.message([src], "Authlist:", this.theme.INFO);
+            var auths = sys.dbAuths();
+            
+            for (var x in auths)
+            {
+                this.com.message([src], "<b>" + ([null, "Moderator", "Admin", "Owner"][sys.dbAuth(auths[x])] || "?") + "</b>" + this.text.escapeHTML(auths[x]) +
+                                 (sys.id(auths[x])?" <span style:'color:green'>online</span>":" <span style:'color:red'>offline</span>"), this.theme.RAW);                
+            }
+        }    
+    }   
+});
