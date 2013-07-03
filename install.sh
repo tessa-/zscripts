435600b636fff9b61b6067c78634b003173f1822
ArchZombie <archzombielord@gmail.com>
2013-07-03 15:27:18 -0400

    Update installer

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-rm -f ${*}/bin/*.js
-for iter in ./*.js
-do
-    ln -s `pwd`/${iter} ${*}/bin/${iter}
-done
+
+if ! test -d "$1"
+then
+    exec echo Provide a directory to install to, e.g.: ./install.sh ../pokemon-online
+else
+    install_to=$1
+    exec echo Installing to $install_to
+    exec ln -s `pwd`/js_modules ${install_to}/bin/js_modules
+    exec cp `pwd`/scripts.js ${install_to}/bin 
+fi
+
+
+    
+#rm -f ${*}/bin/*.js
+#for iter in ./*.js
+#do
+#    ln -s `pwd`/${iter} ${*}/bin/${iter}
+#done
